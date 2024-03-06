88759923badaaba98e26d708fabd7f642a21b680
W Etheredge <me@wetheredge.com>
2024-03-05 19:46:30 -0600

    Move compression into vhs-web build

--- vhs-web/.justfile
+++ vhs-web/.justfile
@@ -17,7 +17,7 @@ dev:
     pnpm vite
 
 build: check
-    pnpm vite build
+    pnpm tsx build.ts
 
 # Run a local server from the latest build artifacts. Needs the vhs-server dev server running
 preview:
--- vhs/build.rs
+++ vhs/build.rs
@@ -1,8 +1,7 @@
+use std::env;
 use std::fs::{self, File};
-use std::io::{self, BufReader, Read, Write};
-use std::path::{Path, PathBuf};
+use std::io::{self, Write};
 use std::process::Command;
-use std::{env, fmt};
 
 fn main() -> io::Result<()> {
     let out_dir = env::var("OUT_DIR").unwrap();
@@ -48,100 +47,48 @@ fn build_info(out_dir: &str, root: &str) -> io::Result<()> {
 }
 
 fn web_assets(out_dir: &str, root: &str) -> io::Result<()> {
-    let compressed_dir = PathBuf::from(format!("{out_dir}/compressed-web-assets"));
-
-    #[derive(Debug)]
-    struct FileInfo {
-        path: PathBuf,
-        content_type: &'static str,
+    #[derive(Debug, serde::Deserialize)]
+    struct Asset {
+        route: String,
+        file: String,
+        mime: String,
         gzip: bool,
-    }
-
-    impl fmt::Display for FileInfo {
-        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-            let headers = if self.gzip {
-                r#"("Content-Encoding", "gzip")"#
-            } else {
-                ""
-            };
-
-            write!(
-                f,
-                r#"::picoserve::response::fs::File::with_content_type_and_headers({:?}, include_bytes!({:?}), &[{headers}])"#,
-                self.content_type, self.path
-            )
-        }
     }
 
     let Ok(web) = fs::canonicalize(format!("{root}/../vhs-web/dist")) else {
         panic!("vhs-web must be built first")
     };
-    let web = web.to_str().unwrap();
-    println!("cargo:rerun-if-changed={web}");
+    println!("cargo:rerun-if-changed={}", web.display());
 
-    let new_file_info = |path: &Path| -> io::Result<FileInfo> {
-        let content_type = match path.extension().map(|s| s.to_string_lossy()).as_deref() {
-            Some("css") => "text/css",
-            Some("html") => "text/html; charset=UTF-8",
-            Some("js") => "text/javascript",
-            Some("svg") => "image/svg+xml",
-            extension => todo!("unknown file extension: `{extension:?}`"),
-        };
-
-        let raw_len = {
-            let mut data = Vec::new();
-            File::open(path)?.read_to_end(&mut data)?;
-            data.len()
-        };
-
-        let mut compressed = Vec::new();
-        let reader = File::open(path)?;
-        let reader = BufReader::new(reader);
-        let mut reader = flate2::bufread::GzEncoder::new(reader, flate2::Compression::best());
-        reader.read_to_end(&mut compressed)?;
-
-        let gzip = compressed.len() < raw_len;
-
-        let path = if gzip {
-            let path = compressed_dir.join(path.strip_prefix(web).unwrap());
-            fs::create_dir_all(path.parent().unwrap())?;
-            fs::write(&path, compressed)?;
-            path
-        } else {
-            path.to_path_buf()
-        };
-
-        Ok(FileInfo {
-            path,
-            content_type,
-            gzip,
-        })
-    };
-
-    let mut files = Vec::new();
-    for entry in walkdir::WalkDir::new(web).sort_by_file_name() {
-        let entry = entry.unwrap();
-        let path = entry.path();
-
-        if path.is_dir() {
-            continue;
-        }
-
-        let path = path.strip_prefix(web).unwrap();
-        let path = path.to_string_lossy();
-        let path = path.trim_end_matches("index.html");
-        let path = path.trim_end_matches(".html");
-
-        files.push((path.to_owned(), new_file_info(entry.path())?));
-    }
+    let assets = fs::read_to_string(web.join("assets.json"))?;
+    let assets: Vec<Asset> = serde_json::from_str(&assets).unwrap();
 
     let out = format!("{out_dir}/router.rs");
     let mut out = File::create(out)?;
     writeln!(&mut out, "macro_rules! router {{")?;
     writeln!(&mut out, "    ($($route:literal => $handler:expr)*) => {{")?;
     writeln!(&mut out, "    ::picoserve::Router::new()")?;
-    for (name, file) in files {
-        writeln!(&mut out, "        .route(\"/{name}\", get(|| {file}))")?;
+    for Asset {
+        route,
+        file,
+        mime,
+        gzip,
+    } in assets
+    {
+        let headers = if gzip {
+            r#"("Content-Encoding", "gzip")"#
+        } else {
+            ""
+        };
+
+        let path = web.join(file);
+
+        writeln!(
+            &mut out,
+            "        .route({route:?}, get(|| \
+             ::picoserve::response::fs::File::with_content_type_and_headers({mime:?}, \
+             include_bytes!({path:?}), &[{headers}])))"
+        )?;
     }
     writeln!(&mut out, "        $( .route($route, $handler) )*")?;
     writeln!(&mut out, "    }};")?;
