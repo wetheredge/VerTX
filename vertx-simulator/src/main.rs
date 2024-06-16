d72d71410a88cb7f10420269330cc18e57787193
W Etheredge <me@wetheredge.com>
2024-06-16 00:04:15 -0500

    Remove unused tracing setup from simulator

--- vertx-simulator/src/main.rs
+++ vertx-simulator/src/main.rs
@@ -4,8 +4,6 @@ mod ui;
 use std::env;
 use std::path::PathBuf;
 
-use tracing_subscriber::EnvFilter;
-
 fn get_target_dir() -> PathBuf {
     let dir = env::current_dir().unwrap();
     let mut dir = dir.as_path();
@@ -21,11 +19,6 @@ fn get_target_dir() -> PathBuf {
 
 #[tokio::main]
 async fn main() {
-    tracing_subscriber::fmt()
-        .pretty()
-        .with_env_filter(EnvFilter::from_env("VERTX_SIMULATOR_LOG"))
-        .init();
-
     relm4_icons::initialize_icons();
     let app = relm4::RelmApp::new("vertx.simulator");
     app.run::<ui::App>(());
