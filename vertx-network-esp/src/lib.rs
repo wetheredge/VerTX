e2c6a3c66f8b4cacb499548de6f08f2eea4daf45
W Etheredge <me@wetheredge.com>
2024-09-14 19:38:10 -0500

    New nightly feature impl_trait_in_assoc_type

--- vertx-network-esp/src/lib.rs
+++ vertx-network-esp/src/lib.rs
@@ -1,4 +1,5 @@
 #![no_std]
+#![feature(impl_trait_in_assoc_type)]
 #![feature(type_alias_impl_trait)]
 #![allow(missing_debug_implementations)]
 
--- vertx/src/main.rs
+++ vertx/src/main.rs
@@ -1,6 +1,7 @@
 #![no_std]
 #![no_main]
 #![feature(asm_experimental_arch)]
+#![feature(impl_trait_in_assoc_type)]
 #![feature(type_alias_impl_trait)]
 
 use core::mem::MaybeUninit;
