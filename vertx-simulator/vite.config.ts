77e73fe12b0648682de321f0c45eb057f64003d3
W Etheredge <me@wetheredge.com>
2024-10-23 00:01:05 -0500

    simulator: Drop unnecessary dep vite-plugin-wasm

--- vertx-simulator/vite.config.ts
+++ vertx-simulator/vite.config.ts
@@ -1,14 +1,9 @@
 import { defineConfig } from 'vite';
-import wasm from 'vite-plugin-wasm';
 
 // biome-ignore lint/style/noDefaultExport: Required by Vite
 export default defineConfig({
 	build: {
 		target: 'esnext',
-	},
-	worker: {
-		format: 'es',
-		plugins: () => [wasm()],
 	},
 	cacheDir: '.vite',
 });
