2a67f60fc1222e5d03a0449a10267e7dcb9da747
W Etheredge <me@wetheredge.com>
2024-11-24 17:27:47 -0500

    Add check targets task & ci job

---
+++ scripts/check-targets.ts
@@ -0,0 +1,55 @@
+#!/usr/bin/env bun
+
+import { exit } from 'node:process';
+import Ajv, { type SomeJTDSchemaType } from 'ajv/dist/jtd';
+import { Glob } from 'bun';
+import { getRepoRoot } from './utils';
+
+const schema: SomeJTDSchemaType = {
+	definitions: {
+		pin: { type: 'uint8' },
+	},
+	properties: {
+		chip: { type: 'string' },
+		pins: {
+			properties: {
+				mode: { ref: 'pin' },
+				leds: { ref: 'pin' },
+				analog: { elements: { ref: 'pin' } },
+				switches: { elements: { ref: 'pin' } },
+			},
+			optionalProperties: {
+				backpack: {
+					properties: {
+						tx: { ref: 'pin' },
+						rx: { ref: 'pin' },
+					},
+				},
+			},
+		},
+	},
+};
+
+const ajv = new Ajv();
+const validate = ajv.compile(schema);
+
+const root = await getRepoRoot();
+const targets = new Glob('targets/*.toml').scan({ cwd: root });
+let allValid = true;
+for await (const path of targets) {
+	const absolutePath = `${root}/${path}`;
+	const data = await import(absolutePath);
+	if (!validate(data.default)) {
+		allValid = false;
+		console.log(path);
+		for (const error of validate.errors ?? []) {
+			console.log(
+				`  ${error.instancePath} ${error.message ?? 'unknown error'}`,
+			);
+		}
+	}
+}
+
+if (!allValid) {
+	exit(1);
+}
