0cf517449963b164cb9b45eafd4bc73044a33c83
W Etheredge <me@wetheredge.com>
2024-12-11 21:43:06 -0500

    backpack: Update for `esp-hal` v0.21

--- vertx-backpack/src/main.rs
+++ vertx-backpack/src/main.rs
@@ -9,16 +9,11 @@ mod api;
 mod ipc;
 mod network;
 
-use core::mem::MaybeUninit;
-
 use embassy_executor::Spawner;
 use esp_backtrace as _;
-use esp_hal::clock::ClockControl;
 use esp_hal::gpio;
-use esp_hal::peripherals::Peripherals;
 use esp_hal::prelude::*;
 use esp_hal::rng::Rng;
-use esp_hal::system::SystemControl;
 use esp_hal::timer::timg;
 use esp_hal::uart::config::Config as UartConfig;
 use esp_hal::uart::Uart;
@@ -27,50 +22,34 @@ use static_cell::StaticCell;
 
 use self::api::Api;
 
-#[global_allocator]
-static ALLOCATOR: esp_alloc::EspHeap = esp_alloc::EspHeap::empty();
-
-/// Initialize the heap
-///
-/// # Safety
-///
-/// Must be called exactly once, before any allocations
-pub unsafe fn init_heap() {
-    const HEAP_SIZE: usize = 32 * 1024;
-    static mut HEAP: MaybeUninit<[u8; HEAP_SIZE]> = MaybeUninit::uninit();
-
-    // SAFETY:
-    // - `init_heap` is required to be called exactly once, before any allocations
-    // - `HEAP_SIZE` is > 0
-    unsafe { ALLOCATOR.init(HEAP.as_mut_ptr() as *mut u8, HEAP_SIZE) };
-}
-
 #[main]
 async fn main(spawner: Spawner) {
-    // SAFETY: main() will only run once
-    unsafe { init_heap() };
+    esp_alloc::heap_allocator!(32 * 1024);
 
     esp_println::logger::init_logger(log::LevelFilter::Info);
     log::info!("Logger initialized");
 
-    let peripherals = Peripherals::take();
-    let system = SystemControl::new(peripherals.SYSTEM);
-    let clocks = ClockControl::max(system.clock_control).freeze();
+    let peripherals = esp_hal::init({
+        let mut config = esp_hal::Config::default();
+        config.cpu_clock = CpuClock::max();
+        config
+    });
+
     let io = gpio::Io::new(peripherals.GPIO, peripherals.IO_MUX);
     let rng = Rng::new(peripherals.RNG);
-    let timg0 = timg::TimerGroup::new(peripherals.TIMG0, &clocks);
-    let timg1 = timg::TimerGroup::new(peripherals.TIMG1, &clocks);
+    let timg0 = timg::TimerGroup::new(peripherals.TIMG0);
+    let timg1 = timg::TimerGroup::new(peripherals.TIMG1);
 
-    esp_hal_embassy::init(&clocks, timg0.timer0);
+    esp_hal_embassy::init(timg0.timer0);
 
     #[cfg(feature = "chip-esp32")]
-    let (tx, rx) = (io.pins.gpio16, io.pins.gpio17);
+    let (rx, tx) = (io.pins.gpio17, io.pins.gpio16);
     #[cfg(feature = "chip-esp32c3")]
-    let (tx, rx) = (io.pins.gpio21, io.pins.gpio20);
+    let (rx, tx) = (io.pins.gpio20, io.pins.gpio21);
     #[cfg(feature = "chip-esp32s3")]
-    let (tx, rx) = (io.pins.gpio4, io.pins.gpio5);
+    let (rx, tx) = (io.pins.gpio5, io.pins.gpio4);
     let config = UartConfig::default().baudrate(vertx_backpack_ipc::BAUDRATE);
-    let mut uart = Uart::new_async_with_config(peripherals.UART1, config, &clocks, tx, rx).unwrap();
+    let mut uart = Uart::new_async_with_config(peripherals.UART1, config, rx, tx).unwrap();
 
     #[ram(rtc_fast, persistent)]
     static BOOT_MODE: AtomicU8 = AtomicU8::new(0);
@@ -81,7 +60,6 @@ async fn main(spawner: Spawner) {
 
     let network = vertx_network_esp::Hal::new(
         spawner,
-        clocks,
         rng,
         timg1.timer0.into(),
         peripherals.RADIO_CLK,
@@ -91,7 +69,7 @@ async fn main(spawner: Spawner) {
     let api = API.init_with(|| Api::new(ipc));
     let start_network = network::get_start(spawner, rng, network, api, ipc);
 
-    let (tx, rx) = uart.split();
-    spawner.must_spawn(ipc::tx(tx, ipc));
+    let (rx, tx) = uart.split();
     spawner.must_spawn(ipc::rx(rx, &BOOT_MODE, start_network, api, ipc));
+    spawner.must_spawn(ipc::tx(tx, ipc));
 }
