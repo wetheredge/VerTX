0e33f9b8eabe22abc0a3f5f8bbc5b785bec925a5
Wren Etheredge <me@wetheredge.com>
2025-02-19 20:55:33 -0500

    Remove support for list dividers

--- vertx/src/ui/component/list.rs
+++ vertx/src/ui/component/list.rs
@@ -4,7 +4,7 @@ use core::cmp::Ordering;
 use embedded_graphics::geometry::Point;
 use embedded_graphics::pixelcolor::BinaryColor;
 use embedded_graphics::prelude::*;
-use embedded_graphics::primitives::{Line, PrimitiveStyle, Rectangle};
+use embedded_graphics::primitives::Rectangle;
 use embedded_graphics::text::{Baseline, Text};
 use embedded_mogeefont::MogeeTextStyle;
 
@@ -20,9 +20,9 @@ pub(in crate::ui) struct List<A: Clone + 'static> {
 }
 
 #[derive(Debug, Clone)]
-pub(in crate::ui) enum Item<A> {
-    Text { label: Cow<'static, str>, action: A },
-    Divider,
+pub(in crate::ui) struct Item<A> {
+    label: Cow<'static, str>,
+    action: A,
 }
 
 impl<A: Clone + 'static> List<A> {
@@ -41,10 +41,7 @@ impl<A: Clone + 'static> List<A> {
     }
 
     pub(in crate::ui) fn set_items(&mut self, items: Cow<'static, [Item<A>]>) {
-        self.selected = items
-            .iter()
-            .position(Item::is_selectable)
-            .unwrap_or_default();
+        self.selected = 0;
         self.scrolling.set_height(items.len() as u32 * LINE_HEIGHT);
         self.items = items;
     }
@@ -54,12 +51,9 @@ impl<A: Clone + 'static> List<A> {
     }
 
     /// Get the action associated with the currently selected item. Returns
-    /// `None` if there aren't any selectable items.
+    /// `None` if the list is empty.
     pub(in crate::ui) fn selected(&self) -> Option<&A> {
-        match self.items.get(self.selected) {
-            Some(Item::Text { action, .. }) => Some(action),
-            Some(Item::Divider) | None => None,
-        }
+        self.items.get(self.selected).map(|item| &item.action)
     }
 
     pub(in crate::ui) fn select_up(&mut self) {
@@ -68,25 +62,12 @@ impl<A: Clone + 'static> List<A> {
             return;
         }
 
-        let mut next = self.selected;
-        loop {
-            if next == 0 {
-                next = self.items.len();
-            }
-            next -= 1;
-
-            if next == self.selected {
-                // Wrapped around to the same entry, so cancel scroll whether or not this is
-                // actually selectable
-                return;
-            }
-
-            if self.items[next].is_selectable() {
-                break;
-            }
-        }
-
-        self.scroll(next);
+        let current = if self.selected == 0 {
+            self.items.len()
+        } else {
+            self.selected
+        };
+        self.scroll(current - 1);
     }
 
     pub(in crate::ui) fn select_down(&mut self) {
@@ -95,22 +76,7 @@ impl<A: Clone + 'static> List<A> {
             return;
         }
 
-        let mut next = self.selected;
-        loop {
-            next = (next + 1) % self.items.len();
-
-            if next == self.selected {
-                // Wrapped around to the same entry, so cancel scroll whether or not this is
-                // actually selectable
-                return;
-            }
-
-            if self.items[next].is_selectable() {
-                break;
-            }
-        }
-
-        self.scroll(next);
+        self.scroll((self.selected + 1) % self.items.len());
     }
 
     fn scroll(&mut self, to: usize) {
@@ -129,17 +95,10 @@ impl<A: Clone + 'static> List<A> {
 }
 
 impl<A: Clone + 'static> Item<A> {
-    pub(in crate::ui) const fn const_text(label: &'static str, action: A) -> Self {
-        Self::Text {
+    pub(in crate::ui) const fn new_const(label: &'static str, action: A) -> Self {
+        Self {
             label: Cow::Borrowed(label),
             action,
-        }
-    }
-
-    fn is_selectable(&self) -> bool {
-        match self {
-            Item::Text { .. } => true,
-            Item::Divider => false,
         }
     }
 }
@@ -178,41 +137,23 @@ impl<A: Clone + 'static> Drawable for List<A> {
         for (i, item) in self.items.iter().enumerate().skip(skip).take(count) {
             let y = i as i32 * LINE_HEIGHT as i32 - scroll_offset as i32;
 
-            match item {
-                Item::Text { label, .. } => {
-                    let style = if i == self.selected && self.selection_visible {
-                        let width = target.bounding_box().size.width;
-                        target.fill_solid(
-                            &Rectangle::new(Point::new(0, y), Size::new(width, LINE_HEIGHT)),
-                            BinaryColor::On,
-                        )?;
+            let style = if i == self.selected && self.selection_visible {
+                let width = target.bounding_box().size.width;
+                target.fill_solid(
+                    &Rectangle::new(Point::new(0, y), Size::new(width, LINE_HEIGHT)),
+                    BinaryColor::On,
+                )?;
 
-                        style_selected
-                    } else {
-                        style
-                    };
+                style_selected
+            } else {
+                style
+            };
 
-                    // Offset to make initial verticals on the inverted item clearer
-                    let position = Point::new(1, y);
+            // Offset to make initial verticals on the inverted item clearer
+            let position = Point::new(1, y);
 
-                    let text = Text::with_baseline(label, position, style, Baseline::Top);
-                    text.draw(&mut target)?;
-                }
-                Item::Divider => {
-                    let y = y + LINE_HEIGHT as i32 / 2;
-                    let thickness = (LINE_HEIGHT % 2) + 1;
-
-                    let gap = thickness;
-                    let length = 8;
-                    let mut x = gap as i32;
-                    for _ in 0..3 {
-                        let line = Line::new(Point::new(x, y), Point::new(x + length - 1, y))
-                            .into_styled(PrimitiveStyle::with_stroke(BinaryColor::On, thickness));
-                        line.draw(&mut target)?;
-                        x += length + gap as i32;
-                    }
-                }
-            }
+            let text = Text::with_baseline(&item.label, position, style, Baseline::Top);
+            text.draw(&mut target)?;
         }
 
         Ok(())
