d06f4f47fa01c36db484f89ee25756a98af2052a
Wren Etheredge <me@wetheredge.com>
2025-10-11 01:18:53 -0400

    script(splash-convert): Replace sharp with fast-png

--- scripts/splash-convert.ts
+++ scripts/splash-convert.ts
@@ -1,34 +1,35 @@
 #!/usr/bin/env bun
 
-import sharp from 'sharp';
+import { type DecodedPng, decode } from 'fast-png';
 import { panic } from './utils.ts';
 
 const WIDTH = 128;
 const HEIGHT = 64;
+
+// Largest power of 2 that can safely be represented in a JS number
+const SAFE_BITS = 16;
 
 const path = Bun.argv[2];
 if (!path) {
 	panic('Missing input path');
 }
 
-const image = sharp(path);
-const meta = await image.metadata();
-if (meta.width !== WIDTH || meta.height !== HEIGHT) {
+const rawImage = await Bun.file(path).arrayBuffer();
+const image = decode(rawImage, { checkCrc: true });
+if (image.width !== WIDTH || image.height !== HEIGHT) {
 	panic('Incorrect dimensions');
 }
 
-const pixels = await image.raw().toColorspace('b-w').toBuffer();
 const output = Bun.stdout.writer();
-output.write('const SPLASH: [u128;64] = [\n');
+output.write('static SPLASH: [u128;64] = [\n');
 for (let i = 0; i < WIDTH * HEIGHT; ) {
 	if (i % WIDTH === 0) {
 		output.write('    0x');
 	}
 
 	const bits: Array<number> = [];
-	for (; bits.length < 16; i++) {
-		// biome-ignore lint/style/noNonNullAssertion: known to be non-null after checking the image dimensions above
-		bits.push(Math.trunc(pixels.at(i)! / 255));
+	for (; bits.length < SAFE_BITS; i++) {
+		bits.push(getBinaryPixel(image, i));
 	}
 	output.write(bitsToHex(bits));
 
@@ -37,6 +38,16 @@ for (let i = 0; i < WIDTH * HEIGHT; ) {
 	}
 }
 output.write('];\n');
+
+function getBinaryPixel(image: DecodedPng, pixel: number): number {
+	// Maximum channel value for source image bit depth
+	const max = 2 ** image.depth - 1;
+
+	const start = pixel * image.channels;
+	const end = (pixel + 1) * image.channels;
+	const monochrome = average(...image.data.slice(start, end));
+	return monochrome / max;
+}
 
 /** Collect (big-endian) bits into a hexadecimal string */
 function bitsToHex(bits: Array<number>): string {
@@ -49,4 +60,12 @@ function bitsToHex(bits: Array<number>): string {
 			.toString(baseHex)
 			.padStart(bits.length / bitsPerHexChar, '0')
 	);
+}
+
+function average(...values: Array<number>): number {
+	if (values.length === 0) {
+		throw new RangeError('Cannot average zero inputs');
+	}
+
+	return values.reduce((acc, curr) => acc + curr) / values.length;
 }
